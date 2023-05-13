d4cc97f082c2b601036d9f544a89b3d5abf08f5a
pallavi <125701543+pallavinet@users.noreply.github.com>
2023-05-14 00:00:43 +0530

    commit1

--- script.js
+++ script.js
@@ -8,20 +8,51 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  const ans1=arr.map(a=>{
+    
+    return a.marks>50;
+  
+  
+})
+console.log(ans1);
+  
+  
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  const ans2=arr.filter(a=>{
+    
+    return a.marks>50;
+  
+  
+})
+console.log(ans2);
 }
 
+let arrnew={id:4,name:"susan",age:"20",marks:45};
 function addData() {
   //Write your code here, just console.log
+  const ans3=arr.push(arrnew);
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  const index=arr.findIndex(a =>{
+    return a.marks<50;
+  })
+  const ans4=arr.splice(index);
+  console.log(ans4);
 }
 
+let arr2 = [
+  { id: 6, name: "jane", age: "52", profession: "admin" },
+  { id: 7, name: "jill", age: "62", profession: "intern" },
+  { id: 8, name: "kin", age: "72", profession: "developer" },
+];
 function concatenateArray() {
   //Write your code here, just console.log
+  const ans5 =arr.concat(arr2);
+  console.log(ans5);
 }
